b47c68832884142c25f8c696bd4116c86afd9d69
delvin <delvinru@gmail.com>
2022-01-07 21:25:09 +0300

    Some changes

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -10,6 +10,12 @@ vim.g.maplocalleader = ','
 -- NERDTree bar
 map('n', '<C-n>', ':NERDTreeToggle<CR>', opts)
 
+-- Better navigation
+map('n', '<C-h>', '<C-w>h', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+map('n', '<C-l>', '<C-w>l', opts)
+
 -- Normal bindings for resize panes
 map('n', '<C-Up>', ':resize -2<CR>', opts)
 map('n', '<C-Down>', ':resize +2<CR>', opts)
